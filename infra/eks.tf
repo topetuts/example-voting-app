39e16ce1b2c3273731a5698dd181e520bd3cfd18
Christopher Llamas <christopher.llamas@avanade.com>
2024-08-08 09:25:01 +0200

    test plna

--- infra/eks.tf
+++ infra/eks.tf
@@ -13,21 +13,21 @@ resource "aws_eks_cluster" "dev-eks" {
 
 }
 
-resource "aws_eks_node_group" "dev-node-group" {
-  cluster_name    = aws_eks_cluster.dev-eks.name
-  node_group_name = "dev-node-group"
-  node_role_arn   = data.aws_iam_role.dev-role.arn
-  subnet_ids      = [aws_subnet.dev-subnet1.id, aws_subnet.dev-subnet2.id]
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 2
-  }
+# resource "aws_eks_node_group" "dev-node-group" {
+#   cluster_name    = aws_eks_cluster.dev-eks.name
+#   node_group_name = "dev-node-group"
+#   node_role_arn   = data.aws_iam_role.dev-role.arn
+#   subnet_ids      = [aws_subnet.dev-subnet1.id, aws_subnet.dev-subnet2.id]
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 2
+#   }
 
-  update_config {
-    max_unavailable = 1
-  }
+#   update_config {
+#     max_unavailable = 1
+#   }
 
-  instance_types = ["t3.medium"]
+#   instance_types = ["t3.medium"]
 
-}
+# }
